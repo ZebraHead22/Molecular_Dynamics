482cd66abd4eb65a7f7901608c741a45e82ee0fe
ZebraHead22 <baranovma1993@gmail.com>
2022-07-27 14:46:53 +0300

    prom

---
+++ Processing/averageSpec.py
@@ -0,0 +1,39 @@
+import os
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
+folders = os.listdir(os.getcwd())
+for folder in folders:
+    if os.path.isdir(folder) == True:       
+        #Define variable
+        av = pd.DataFrame()
+        dirPath = os.getcwd()+'/'+folder
+        files = os.listdir(dirPath)
+        print(dirPath)
+        #Write data into one file
+        for i in files:
+            filename, file_extension = os.path.splitext(dirPath+'/'+i)
+            if file_extension == ".dat":
+                print(i)
+                df = pd.read_csv(dirPath+'/'+i, delimiter=' ', index_col=None)
+                df.rename(columns = {'0.0' : 'Freq', '0.0.1' : 'Amplitude'}, inplace = True)
+                av['Freq'] = df['Freq']
+                av[os.path.basename(i)] = df['Amplitude']
+                with pd.ExcelWriter(str(dirPath)+"/"+'result.xlsx') as writer:
+                    av.to_excel(writer, sheet_name='average data', index=None, index_label=None)
+        #Make average amplitude data
+        av_average = av.drop('Freq', axis = 1)
+        av_average = np.array(av_average.mean(axis = 1))
+        av_freq = np.array(av['Freq'])
+        #Make plot
+        plt.gcf().clear()
+        plt.plot(av_freq, av_average)
+        plt.ylabel('Spectral Density (a.u.)')
+        plt.xlabel('Frequency ($cm^{-1}$)')
+        plt.grid()
+        plt.savefig(dirPath+'/'+os.path.basename(dirPath)+'_average.png')
+        print('Picture saved...')
+    else:
+        pass
+        continue
