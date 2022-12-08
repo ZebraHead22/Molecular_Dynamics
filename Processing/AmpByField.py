76d225a7cd7fc0f42fbdde0a1099eb4a4c1a3ea0
Maksim Baranov <baranovma1993@gmail.com>
2022-12-08 12:42:18 +0300

    2 defs

--- Processing/AmpByField.py
+++ Processing/AmpByField.py
@@ -4,50 +4,91 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-av = pd.DataFrame()
-frequency = []
-amplitude = []
-list_files = []
+def one_experiment():
+    av = pd.DataFrame()
+    field_amplutide = []
+    amplitude = []
+    list_files = []
 
-folders = os.listdir(os.getcwd())
-for folder in folders:
-    if os.path.isdir(folder) == True:     
-        dirPath = os.getcwd()+'/'+folder
-        files = os.listdir(dirPath)
-        for i in files:
-            filename, file_extension = os.path.splitext(dirPath+'/'+i)
-            if file_extension == ".dat":
-                print(i)
-                df = pd.read_csv(dirPath+'/'+i, delimiter=' ', index_col=None)
-                df.rename(columns = {'0.0' : 'Frequency', '0.0.1' : 'Amplitude'}, inplace = True)
-                print(df.head())
-                dfFreq = np.array(df['Frequency'].tolist())
-                dfAmp = np.array(df['Amplitude'].tolist())
-                #make file xlsx
-                with pd.ExcelWriter(filename+'.xlsx') as writer:
-                    df.to_excel(writer, sheet_name='Spectral Dencity', index=None, index_label=None)
-                #Make png
-                plt.gcf().clear()
-                plt.plot(dfFreq, dfAmp)
-                plt.ylabel('Spectral Density (a.u.)')
-                plt.xlabel('Frequency ($cm^{-1}$)')
-                plt.grid()
-                plt.savefig(filename+'.png')
-                print('Picture saved'+' '+filename+'.png')
-                #Find max over 3333cm-1 and add to list
-                df = df.where(df['Amplitude'].max()&df['Frequency']>3000)
-                # frequency.append(df['Frequency'].where(df['Amplitude'].max()&df['Frequency']>3000))
-                # amplitude.append(df['Amplitude'].where(df['Frequency']>3000).max())
-        # exp = pd.DataFrame(list(zip(frequency, amplitude)), columns =['Frequency', 'Spectral Density (a.u.)'])
-        # print(exp)
-        # with pd.ExcelWriter(dirPath+'/'+'dependence.xlsx') as writer:
-        #     exp.to_excel(writer, sheet_name='Dependence', index=None, index_label=None)
+    folders = os.listdir(os.getcwd())
+    for folder in folders:
+        if os.path.isdir(folder) == True:     
+            dirPath = os.getcwd()+'/'+folder
+            files = os.listdir(dirPath)
+            for i in files:
+                filename, file_extension = os.path.splitext(dirPath+'/'+i)
+                if file_extension == ".dat":
+                    print(i)
+                    df = pd.read_csv(dirPath+'/'+i, delimiter=' ', index_col=None)
+                    df.rename(columns = {'0.0' : 'Frequency', '0.0.1' : 'Amplitude'}, inplace = True)
+                    print(df.head())
+                    dfFreq = np.array(df['Frequency'].tolist())
+                    dfAmp = np.array(df['Amplitude'].tolist())
+                    #make file xlsx
+                    with pd.ExcelWriter(filename+'.xlsx') as writer:
+                        df.to_excel(writer, sheet_name='Spectral Dencity', index=None, index_label=None)
+                    #Make png
+                    plt.gcf().clear()
+                    plt.plot(dfFreq, dfAmp)
+                    plt.ylabel('Spectral Density (a.u.)')
+                    plt.xlabel('Frequency ($cm^{-1}$)')
+                    plt.grid()
+                    plt.savefig(filename+'.png')
+                    print('Picture saved'+' '+filename+'.png')
+                    #Find max over 3333cm-1 and add to list
+                    field_amplutide.append(float(folder))
+                    amplitude.append(df['Amplitude'].where(df['Frequency']>3000).max())
+    exp = pd.DataFrame(list(zip(field_amplutide, amplitude)), columns =['Field Fmplutide (kcal/mol*A*e)', 'Spectral Density (a.u.)'])
+    with pd.ExcelWriter('dependence.xlsx') as writer:
+        exp.to_excel(writer, sheet_name='Dependence', index=None, index_label=None)
+
+def few_experiments():
+    av = pd.DataFrame()
+    field_amplutide = []
+    amplitude = []
+    list_files = []
+
+    folders = os.listdir(os.getcwd())
+    for folder in folders:
+        if os.path.isdir(folder) == True:       
+            #Define variable
+            dirPath = os.getcwd()+'/'+folder
+            folders_2 = os.listdir(dirPath)
+            for i in folders_2:
+                exp_folder = dirPath+'/'+i
+                files = os.listdir(exp_folder)
+                for a in files:
+                    # filename, file_extension = os.path.splitext(dirPath+'/'+a)
+                    # if file_extension == ".dat":
+                    #     print(a)
+                    #     df = pd.read_csv(exp_folder+'/'+a, delimiter=' ', index_col=None)
+                    #     df.rename(columns = {'0.0' : 'Frequency', '0.0.1' : 'Amplitude'}, inplace = True)
+                    #     print(df.head())
+                    #     dfFreq = np.array(df['Frequency'].tolist())
+                    #     dfAmp = np.array(df['Amplitude'].tolist())
+                    #     #make file xlsx
+                    #     with pd.ExcelWriter(filename+'.xlsx') as writer:
+                    #         df.to_excel(writer, sheet_name='Spectral Dencity', index=None, index_label=None)
+                    #     #Make png
+                    #     plt.gcf().clear()
+                    #     plt.plot(dfFreq, dfAmp)
+                    #     plt.ylabel('Spectral Density (a.u.)')
+                    #     plt.xlabel('Frequency ($cm^{-1}$)')
+                    #     plt.grid()
+                    #     plt.savefig(filename+'.png')
+                    #     print('Picture saved'+' '+filename+'.png')
+                    #     #Find max over 3333cm-1 and add to list
+                    #     field_amplutide.append(float(folder))
+                    #     amplitude.append(df['Amplitude'].where(df['Frequency']>3000).max())
+         
 
 
-print(frequency)
-  
+                
 
 
+
+
+few_experiments()
 # for folder in folders:
 #     if os.path.isdir(folder) == True: 
 #         df = pd.read_excel(os.getcwd()+'/'+folder+'/'+'dependence.xlsx')
