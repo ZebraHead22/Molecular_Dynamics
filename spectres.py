358c66d8b45a4a9df76ba2e0caec421c9f062bcb
ZebraHead22 <baranovma1993@gmail.com>
2023-01-20 16:49:21 +0300

    spectres add

---
+++ spectres.py
@@ -0,0 +1,30 @@
+#Здесь строим кучу спектров из набора дат файлов в одной папке
+
+import os
+import re
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+av = pd.DataFrame()
+field_amplutide = []
+amplitude = []
+list_files = []
+
+files = os.listdir(os.getcwd())
+for i in files:
+    filename, file_extension = os.path.splitext(os.getcwd()+'/'+i)
+    df = pd.read_csv(os.getcwd()+'/'+i, delimiter=' ', index_col=None)
+    df.rename(columns = {'0.0' : 'Frequency', '0.0.1' : 'Amplitude'}, inplace = True)
+    print(df.head())
+    dfFreq = np.array(df['Frequency'].tolist())
+    dfAmp = np.array(df['Amplitude'].tolist())
+    #Make png
+    plt.gcf().clear()
+    plt.plot(dfFreq, dfAmp)
+    plt.ylabel('Spectral Density (a.u.)')
+    plt.xlabel('Frequency ($cm^{-1}$)')
+    plt.grid()
+    plt.savefig(filename+'.png')
+    print('Picture saved'+' '+filename+'.png')
