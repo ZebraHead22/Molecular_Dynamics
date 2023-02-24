8f43ce908f20dd781bb92568e49478d651c82f7b
ZebraHead22 <baranovma1993@gmail.com>
2023-02-24 16:14:22 +0300

    spectres

---
+++ external.py
@@ -0,0 +1,39 @@
+import numpy as np
+import scipy as sp
+import pandas as pd
+from pandas import Series
+from scipy import fftpack
+from matplotlib import pyplot as plt
+from scipy.fft import rfft, rfftfreq
+
+time = np.array(list(range(0, 1000000)))
+magnitude = np.array([1, 1, 1, 1, 1, -1, -1, -1 ,-1, -1]*100000)
+
+time1 = np.array(list(range(0, 50)))
+magnitude1 = np.array([1, 1, 1, 1, 1, -1, -1, -1 ,-1, -1]*5)
+
+plt.scatter(time1, magnitude1)
+plt.ylabel('Amplitude (kcal/mol*A*e)')
+plt.xlabel('Time (ps)')
+plt.grid()
+
+# plt.savefig('timeDependence.png')
+
+energies_fft = sp.fftpack.fft(magnitude)
+energies_psd = np.abs(energies_fft)
+fftFreq = sp.fftpack.fftfreq(len(energies_fft), 1/10**15)
+i = fftFreq > 0
+reverseCm = 1/((3*(10**10))/(fftFreq[i]))
+
+plt.gcf().clear()
+plt.plot(reverseCm, energies_psd[i])
+plt.ylabel('Spectral Density (a.u.)')
+plt.xlabel('Frequency ($cm^{-1}$)')
+plt.grid()
+# plt.show()
+data = pd.DataFrame(list(zip(reverseCm, energies_psd[i])), columns=['Frequency', 'Amplitude'])
+# print(data.loc[data['Amplitude'].idxmax()])
+for i in data['Amplitude'].nlargest(2):
+    print(data['Frequency'].where(data['Amplitude']==i).dropna())
+
+
