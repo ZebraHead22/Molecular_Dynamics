d4f4dc38d76c583c1c279e143c464b60c2e7c500
ZebraHead22 <baranovma1993@gmail.com>
2023-03-26 19:40:44 +0300

    nice

--- Processing/filter.py
+++ Processing/filter.py
@@ -9,29 +9,35 @@ from matplotlib import pyplot as plt
 from scipy import signal
 
 
-file = "/Users/max/Yandex.Disk.localized/NamdData/noField/glycine_5fs/dipole.dat"
+file = "C:/Users/baranov_ma/YandexDisk/NamdData/noField/gly_1fs/dipole_gly.dat"
 df = pd.read_csv(file, sep = ' ')
 df.dropna(how='all', axis=1, inplace=True)
 df.rename(columns={'#': 'frame', 'Unnamed: 2': 'dip_x', 'Unnamed: 4': 'dip_y', 'Unnamed: 6': 'dip_z', 'Unnamed: 8': 'dip_abs'}, inplace=True)
 df.insert(1, "Time", (df['frame']*10**(-15)))
-f0 = rfft(df['dip_abs'].tolist())
-f0 = np.abs(f0)
-f1 = f0.copy()
-f1[:3330] = 0
+
+window = np.hanning(int(round(len(df['frame']))))
+ySamp = (df['dip_abs'].tolist()) * window
+
+dipoles = sc.fftpack.fft(np.array(ySamp))
+dipoles = np.abs(dipoles)
+fftFreq = sc.fftpack.fftfreq(len(df['Time'].tolist()), 1*10**(-15))
+i = fftFreq > 0
+reverseCm = 1/((3*(10**10))/(fftFreq[i]))
+
+
+f1 = dipoles.copy()
+f1[:9000] = 0
+f1[11000:]=0
 z = irfft(f1)
 
-fftFreq = sc.fftpack.fftfreq(len(df['Time'].tolist()), 5*10**(-15))
-
-i = fftFreq > 0
-
-reverseCm = 1/((3*(10**10))/(fftFreq[i]))
 
 f, (dip1, dip2) = plt.subplots(2, 1, sharey=False)
 
-dip1.plot(np.array(np.array(fftFreq[i])), f0[i], label='dip1')
+dip1.plot(np.array(reverseCm), f1[i], label='dip1')
 dip1.title.set_text('dip1')
+dip1.axis(xmin=0,xmax=6000)
 
-dip2.plot(np.array(fftFreq[i]), z[i], label='dip2')
+dip2.plot(np.array(df['Time'].tolist()), z, label='dip2')
 dip2.title.set_text('dip2')
-
+dip2.axis(xmin=0.1*10**(-10),xmax=0.9*10**(-10))
 plt.show()
