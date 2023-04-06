866fdc1327bd5de1a80a6c6925de380b2a7c9723
ZebraHead22 <baranovma1993@gmail.com>
2023-04-06 20:12:51 +0300

    normal external

--- Processing/external.py
+++ Processing/external.py
@@ -6,19 +6,22 @@ from scipy import fftpack
 from matplotlib import pyplot as plt
 from scipy.fft import rfft, rfftfreq
 
-time = np.array(list(range(0, 80)))
-magnitude = np.array([1, 1, 1, 1, -1, -1, -1, -1]*10)
+time = np.array(list(range(0, 3000)))
+magnitude = [1, 1, 1, 1, 1, -1, -1, -1, -1, -1]*100
+zeros = [0]*1000
+all = np.array(zeros + magnitude + zeros)
 
-if len(time) == len(magnitude):
+
+if len(time) == len(all):
     print("True")
-    plt.scatter(time, magnitude)
+    plt.scatter(time, all)
     plt.ylabel('Amplitude (kcal/mol*A*e)')
     plt.xlabel('Time (ps)')
     plt.grid()
 
     plt.savefig('timeDependence_4.png')
 
-    energies_fft = sp.fftpack.fft(magnitude)
+    energies_fft = sp.fftpack.fft(all)
     energies_psd = np.abs(energies_fft)
     fftFreq = sp.fftpack.fftfreq(len(energies_fft), 1/10**15)
     i = fftFreq > 0
@@ -29,13 +32,15 @@ if len(time) == len(magnitude):
     plt.ylabel('Spectral Density (a.u.)')
     plt.xlabel('Frequency ($cm^{-1}$)')
     plt.grid()
+    plt.show()
     # plt.show()
     data = pd.DataFrame(list(zip(reverseCm, energies_psd[i])), columns=[
                         'Frequency', 'Amplitude'])
     # print(data.loc[data['Amplitude'].idxmax()])
     for i in data['Amplitude'].nlargest(2):
         print(data['Frequency'].where(data['Amplitude'] == i).dropna())
+
 else:
     print("False")
     print("len time="+str(len(time)))
-    print("len magnitude="+str(len(magnitude)))
+    print("len magnitude="+str(len(all)))
