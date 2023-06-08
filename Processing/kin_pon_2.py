090880b9ca6a9320a17fc113f2a39e1a796a93de
ZebraHead22 <baranovma1993@gmail.com>
2023-06-08 17:01:59 +0300

    spectres

--- Processing/kin_pon_2.py
+++ Processing/kin_pon_2.py
@@ -1,8 +1,12 @@
 import os
 import re
 import numpy as np
+import scipy as sp
 import pandas as pd
+from scipy import signal
+from scipy import fftpack
 from matplotlib import pyplot as plt
+from scipy.fft import rfft, rfftfreq
 
 data = pd.DataFrame()
 directory = os.getcwd()
@@ -19,41 +23,62 @@ for address, dirs, names in os.walk(directory):
             data[(str(filename)+"_Energy")] = one_data["Energy"]
 
 plt.gcf().clear()
-fig, ax = plt.subplots(nrows=1, ncols=2)
+# fig, ax = plt.subplots(nrows=1, ncols=2)
 
+# def cm_to_inch(value):  # Define picture size, calc cm in inch
+#     return value/2.54
 
-def cm_to_inch(value):  # Define picture size, calc cm in inch
-    return value/2.54
+# fig.set_figheight(cm_to_inch(10))
+# fig.set_figwidth(cm_to_inch(30))
 
+# ax[0].scatter(data["2_kin_Time"].tolist(),
+#               data["2_kin_Energy"].tolist(), c='#FFDD19', s=2)
+# ax[0].scatter(data["4_kin_Time"].tolist(),
+#               data["4_kin_Energy"].tolist(), c='#A000FF', s=2)
+# ax[0].scatter(data["6_kin_Time"].tolist(),
+#               data["6_kin_Energy"].tolist(), c='#35FF19', s=2)
+# ax[0].scatter(data["8_kin_Time"].tolist(),
+#               data["8_kin_Energy"].tolist(), c='#220DFF', s=2)
 
-fig.set_figheight(cm_to_inch(10))
-fig.set_figwidth(cm_to_inch(30))
+# ax[1].scatter(data["2_pon_Time"].tolist(),
+#               data["2_pon_Energy"].tolist(), c='#FFDD19', s=2)
+# ax[1].scatter(data["4_pon_Time"].tolist(),
+#               data["4_pon_Energy"].tolist(), c='#A000FF', s=2)
+# ax[1].scatter(data["6_pon_Time"].tolist(),
+#               data["6_pon_Energy"].tolist(), c='#35FF19', s=2)
+# ax[1].scatter(data["8_pon_Time"].tolist(),
+#               data["8_pon_Energy"].tolist(), c='#220DFF', s=2)
 
-ax[0].scatter(data["2_kin_Time"].tolist(),
-              data["2_kin_Energy"].tolist(), c='#FFDD19', s=2)
-ax[0].scatter(data["4_kin_Time"].tolist(),
-              data["4_kin_Energy"].tolist(), c='#A000FF', s=2)
-ax[0].scatter(data["6_kin_Time"].tolist(),
-              data["6_kin_Energy"].tolist(), c='#35FF19', s=2)
-ax[0].scatter(data["8_kin_Time"].tolist(),
-              data["8_kin_Energy"].tolist(), c='#220DFF', s=2)
+# ax[0].set_xlabel('Time (ps)')
+# ax[1].set_xlabel('Time (ps)')
+# ax[0].set_ylabel('Energy (eV)')
+# ax[1].set_ylabel('Energy (eV)')
+# ax[0].grid()
+# ax[1].grid()
+# ax[0].title.set_text('Kinetic')
+# ax[1].title.set_text('Potential')
+# plt.legend(['0.087 V/nm', '0.173 V/nm', '0.260 V/nm', '0.346 V/nm'], markerscale=5.)
+# plt.savefig(directory+'/'+'dependence.png')
 
-ax[1].scatter(data["2_pon_Time"].tolist(),
-              data["2_pon_Energy"].tolist(), c='#FFDD19', s=2)
-ax[1].scatter(data["4_pon_Time"].tolist(),
-              data["4_pon_Energy"].tolist(), c='#A000FF', s=2)
-ax[1].scatter(data["6_pon_Time"].tolist(),
-              data["6_pon_Energy"].tolist(), c='#35FF19', s=2)
-ax[1].scatter(data["8_pon_Time"].tolist(),
-              data["8_pon_Energy"].tolist(), c='#220DFF', s=2)
+energies_psd_kin = np.abs(sp.fftpack.fft(data["2_kin_Energy"].tolist()))
+energies_psd_pon = np.abs(sp.fftpack.fft(data["2_pon_Energy"].tolist()))
+n = len(energies_psd_kin)
+window = sp.signal.windows.gaussian(n, std=5000, sym=False)
+module_w_kin = energies_psd_kin * window
+module_w_pon = energies_psd_pon * window
+b, a = sp.signal.butter(2, 0.05)
+filtered_kin = sp.signal.filtfilt(b, a, module_w_kin)
+filtered_pon = sp.signal.filtfilt(b, a, module_w_pon)
+fftFreq = sp.fftpack.fftfreq(len(data["8_kin_Energy"].tolist()), 1/10**15)
+i = fftFreq > 0
+reverseCm = 1/((3*(10**10))/(fftFreq[i]))
 
-ax[0].set_xlabel('Time (ps)')
-ax[1].set_xlabel('Time (ps)')
-ax[0].set_ylabel('Energy (eV)')
-ax[1].set_ylabel('Energy (eV)')
-ax[0].grid()
-ax[1].grid()
-ax[0].title.set_text('Kinetic')
-ax[1].title.set_text('Potential')
-plt.legend(['0.087 V/nm', '0.173 V/nm', '0.260 V/nm', '0.346 V/nm'], markerscale=5.)
-plt.savefig(directory+'/'+'dependence.png')
+plt.plot(reverseCm, filtered_kin[i], c = 'darkblue')
+plt.plot(reverseCm, filtered_pon[i], c = 'red')
+plt.grid()
+plt.xlabel("Frequency (cm$^{-1}$)")
+plt.ylabel("Amplitude (a.u.)")
+plt.legend(["Kinetic", "Potential"])
+plt.title("0.087 V/nm")
+plt.xlim(13500, 16550)
+plt.savefig(directory+'/'+'dependence_087.png')
