eb7be09df39e20afeb14b19b3dadb34600eaad8f
ZebraHead22 <baranovma1993@gmail.com>
2023-06-13 14:54:10 +0300

    3d

--- Processing/kin_pon_2.py
+++ Processing/kin_pon_2.py
@@ -15,14 +15,55 @@ for address, dirs, names in os.walk(directory):
         filename, file_extension = os.path.splitext(name)
         if file_extension == ".dat":
             one_data = pd.read_csv(os.path.join(
-                address, name), delimiter=' ', index_col=None, header=None)
-            one_data.rename(columns={0: "TS", 1: "Amplitude"}, inplace=True)
+                address, name), delimiter=' ', index_col=None, header=[0])
+
+            # one_data.rename(columns={0: "TS", 1: "Amplitude"}, inplace=True)
             one_data.insert(1, "Time", (one_data["TS"]) * 0.001)
-            one_data.insert(3, "Energy", (one_data["Amplitude"] * 0.0434/5400))
+            one_data.insert(3, "Kinetic", (one_data["KINETIC"] * 0.0434/5400))
+            one_data.insert(3, "Potential", (one_data["POTENTIAL"] * 0.0434/5400))
             data[(str(filename)+"_Time")] = one_data["Time"]
-            data[(str(filename)+"_Energy")] = one_data["Energy"]
+            data[(str(filename)+"_Kinetic")] = one_data["Kinetic"]
+            data[(str(filename)+"_Potential")] = one_data["Potential"]
 
 plt.gcf().clear()
+amplitudes = ['09', '10', '03', '08', '12', '04', '11', '05', '07', '02', '01', '06']
+en_type = ['Kinetic', 'Potential']
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+"""Построение семейтв энергий в шести окнах для всех АК"""
+for i in data.columns.values:
+    amino_acid = re.search(r'^\w{,2}[^\_]', str(i))
+    amino_acid = amino_acid.group(0)
+    if amino_acid == 'trp':
+        for i in amplitudes:
+            ax.bar(data[str(amino_acid)+"_"+str(i)+'_data_Time'], data[(str(amino_acid)+"_"+str(i)+'_data_Kinetic')], alpha=0.8)
+
+
+plt.show()   
+        
+
+# fig, ax = plt.subplots(nrows=3, ncols=2)
+# def cm_to_inch(value):  # Define picture size, calc cm in inch
+#     return value/2.54
+# fig.set_figheight(cm_to_inch(30))
+# fig.set_figwidth(cm_to_inch(10))
+# ax[0].scatter(data["2_kin_Time"].tolist(), data["2_kin_Energy"].tolist(), s=2)
+
+
+# ax[0].set_xlabel('Time (ps)')
+# ax[1].set_xlabel('Time (ps)')
+# ax[0].set_ylabel('Energy (eV)')
+# ax[1].set_ylabel('Energy (eV)')
+# ax[0].grid()
+# ax[1].grid()
+# ax[0].title.set_text('Kinetic')
+# ax[1].title.set_text('Potential')
+# plt.legend(['0.087 V/nm', '0.173 V/nm', '0.260 V/nm', '0.346 V/nm'], markerscale=5.)
+plt.savefig(directory+'/'+'dependence.png')
+
+
+"""Построение семейтв энергий в двух окнах"""
 # fig, ax = plt.subplots(nrows=1, ncols=2)
 
 # def cm_to_inch(value):  # Define picture size, calc cm in inch
@@ -59,26 +100,26 @@ plt.gcf().clear()
 # ax[1].title.set_text('Potential')
 # plt.legend(['0.087 V/nm', '0.173 V/nm', '0.260 V/nm', '0.346 V/nm'], markerscale=5.)
 # plt.savefig(directory+'/'+'dependence.png')
+"""Вычисление спектров энергий"""
+# energies_psd_kin = np.abs(sp.fftpack.fft(data["2_kin_Energy"].tolist()))
+# energies_psd_pon = np.abs(sp.fftpack.fft(data["2_pon_Energy"].tolist()))
+# n = len(energies_psd_kin)
+# window = sp.signal.windows.gaussian(n, std=5000, sym=False)
+# module_w_kin = energies_psd_kin * window
+# module_w_pon = energies_psd_pon * window
+# b, a = sp.signal.butter(2, 0.05)
+# filtered_kin = sp.signal.filtfilt(b, a, module_w_kin)
+# filtered_pon = sp.signal.filtfilt(b, a, module_w_pon)
+# fftFreq = sp.fftpack.fftfreq(len(data["8_kin_Energy"].tolist()), 1/10**15)
+# i = fftFreq > 0
+# reverseCm = 1/((3*(10**10))/(fftFreq[i]))
 
-energies_psd_kin = np.abs(sp.fftpack.fft(data["2_kin_Energy"].tolist()))
-energies_psd_pon = np.abs(sp.fftpack.fft(data["2_pon_Energy"].tolist()))
-n = len(energies_psd_kin)
-window = sp.signal.windows.gaussian(n, std=5000, sym=False)
-module_w_kin = energies_psd_kin * window
-module_w_pon = energies_psd_pon * window
-b, a = sp.signal.butter(2, 0.05)
-filtered_kin = sp.signal.filtfilt(b, a, module_w_kin)
-filtered_pon = sp.signal.filtfilt(b, a, module_w_pon)
-fftFreq = sp.fftpack.fftfreq(len(data["8_kin_Energy"].tolist()), 1/10**15)
-i = fftFreq > 0
-reverseCm = 1/((3*(10**10))/(fftFreq[i]))
-
-plt.plot(reverseCm, filtered_kin[i], c = 'darkblue')
-plt.plot(reverseCm, filtered_pon[i], c = 'red')
-plt.grid()
-plt.xlabel("Frequency (cm$^{-1}$)")
-plt.ylabel("Amplitude (a.u.)")
-plt.legend(["Kinetic", "Potential"])
-plt.title("0.087 V/nm")
-plt.xlim(13500, 16550)
-plt.savefig(directory+'/'+'dependence_087.png')
+# plt.plot(reverseCm, filtered_kin[i], c = 'darkblue')
+# plt.plot(reverseCm, filtered_pon[i], c = 'red')
+# plt.grid()
+# plt.xlabel("Frequency (cm$^{-1}$)")
+# plt.ylabel("Amplitude (a.u.)")
+# plt.legend(["Kinetic", "Potential"])
+# plt.title("0.087 V/nm")
+# plt.xlim(13500, 16550)
+# plt.savefig(directory+'/'+'dependence_087.png')
