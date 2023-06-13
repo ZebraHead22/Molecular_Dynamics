472dd47da4ede03b10b4dbb31566575b6af9ac6f
ZebraHead22 <baranovma1993@gmail.com>
2023-06-13 18:51:27 +0300

    eban

--- Processing/kin_pon_2.py
+++ Processing/kin_pon_2.py
@@ -5,8 +5,18 @@ import scipy as sp
 import pandas as pd
 from scipy import signal
 from scipy import fftpack
+from matplotlib import cm
 from matplotlib import pyplot as plt
 from scipy.fft import rfft, rfftfreq
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import colors as mcolors
+from matplotlib.collections import PolyCollection
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+amino_acids = list()
+energy_type = ['KINETIC', 'POTENTIAL']
+field_amplitudes = [0.0435,	0.087,	0.1305,	0.174,	0.2175,
+                    0.261,	0.3045,	0.348,	0.3915,	0.435,	0.4785,	0.522]
 
 data = pd.DataFrame()
 directory = os.getcwd()
@@ -14,18 +24,68 @@ for address, dirs, names in os.walk(directory):
     for name in names:
         filename, file_extension = os.path.splitext(name)
         if file_extension == ".dat":
-            one_data = pd.read_csv(os.path.join(
-                address, name), delimiter=' ', index_col=None, header=[0])
+            amino_acid = re.search(r'^\w{,2}[^\_]', filename)
+            amino_acid = amino_acid.group(0)
+            amino_acids.append(amino_acid)
+amino_acids = list(set(amino_acids))
 
-            # one_data.rename(columns={0: "TS", 1: "Amplitude"}, inplace=True)
-            one_data.insert(1, "Time", (one_data["TS"]) * 0.001)
-            one_data.insert(3, "Kinetic", (one_data["KINETIC"] * 0.0434/5400))
-            one_data.insert(3, "Potential", (one_data["POTENTIAL"] * 0.0434/5400))
-            data["Time"] = one_data["Time"]
-            # data[(str(filename)+"_Kinetic")] = one_data["Kinetic"]
-            data[(str(filename)+"_Potential")] = one_data["Potential"]
-            print(filename)
-data.to_excel(directory+"/trp_potential.xlsx")
+for i in amino_acids:
+    directory = os.getcwd()
+    directory = directory+"/"+i
+    for j in energy_type:
+        data = pd.DataFrame()
+        for address, dirs, names in os.walk(directory):
+            for name in names:
+                filename, file_extension = os.path.splitext(name)
+                if file_extension == ".dat":
+                    one_data = pd.read_csv(os.path.join(
+                        address, name), delimiter=' ', index_col=None, header=[0])
+                    data["TIME"] = (one_data["TS"]) * 0.001
+                    data[(str(filename)+"_"+j)] = (one_data[j]) * 0.0434/5400
+        # Тут делаем график 1 и 2
+        plt.gcf().clear()
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+       
+        def cc(arg):
+            return mcolors.to_rgba(arg, alpha=0.6)
+
+        xs = np.array(data["TIME"].tolist())
+        verts = []
+        ys = field_amplitudes
+        a = 1
+        for y in ys:
+            zs = data.iloc[:,a].tolist()
+            verts.append(list(zip(xs, zs)))
+            a+=1
+        print(len(verts))
+      
+        poly = PolyCollection(verts, facecolors=[cc('r'), cc('g'), cc('b'),
+                                                cc('y')])
+        poly.set_alpha(0.7)
+        ax.add_collection3d(poly, zs=zs)
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        plt.title(i+'_'+j)
+        plt.show()
+        # plt.savefig(directory+'/'+filename+'_'+j+'.png')
+
+
+#         filename, file_extension = os.path.splitext(name)
+#         if file_extension == ".dat":
+#             one_data = pd.read_csv(os.path.join(
+#                 address, name), delimiter=' ', index_col=None, header=[0])
+
+#             # one_data.rename(columns={0: "TS", 1: "Amplitude"}, inplace=True)
+#             one_data.insert(1, "Time", (one_data["TS"]) * 0.001)
+#             one_data.insert(3, "Kinetic", (one_data["KINETIC"] * 0.0434/5400))
+#             one_data.insert(3, "Potential", (one_data["POTENTIAL"] * 0.0434/5400))
+#             data["Time"] = one_data["Time"]
+#             # data[(str(filename)+"_Kinetic")] = one_data["Kinetic"]
+#             data[(str(filename)+"_Potential")] = one_data["Potential"]
+#             print(filename)
+# data.to_excel(directory+"/trp_potential.xlsx")
 
 """Построение семейтв энергий в двух окнах"""
 # fig, ax = plt.subplots(nrows=1, ncols=2)
