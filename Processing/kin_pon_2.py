76f2c3e5a8bdea1b41001c75c33b1c76b70cd9d5
ZebraHead22 <baranovma1993@gmail.com>
2023-06-14 16:04:35 +0300

    done

--- Processing/kin_pon_2.py
+++ Processing/kin_pon_2.py
@@ -42,28 +42,21 @@ for i in amino_acids:
                         address, name), delimiter=' ', index_col=None, header=[0])
                     data["TIME"] = (one_data["TS"]) * 0.001
                     data[(str(filename)+"_"+j)] = (one_data[j]) * 0.0434/5400
-                    data.to_excel(directory+"_"+i+"_"+j+".xlsx")
-        
-"""Вычисление спектров энергий"""
-# energies_psd_kin = np.abs(sp.fftpack.fft(data["2_kin_Energy"].tolist()))
-# energies_psd_pon = np.abs(sp.fftpack.fft(data["2_pon_Energy"].tolist()))
-# n = len(energies_psd_kin)
-# window = sp.signal.windows.gaussian(n, std=5000, sym=False)
-# module_w_kin = energies_psd_kin * window
-# module_w_pon = energies_psd_pon * window
-# b, a = sp.signal.butter(2, 0.05)
-# filtered_kin = sp.signal.filtfilt(b, a, module_w_kin)
-# filtered_pon = sp.signal.filtfilt(b, a, module_w_pon)
-# fftFreq = sp.fftpack.fftfreq(len(data["8_kin_Energy"].tolist()), 1/10**15)
-# i = fftFreq > 0
-# reverseCm = 1/((3*(10**10))/(fftFreq[i]))
-
-# plt.plot(reverseCm, filtered_kin[i], c = 'darkblue')
-# plt.plot(reverseCm, filtered_pon[i], c = 'red')
-# plt.grid()
-# plt.xlabel("Frequency (cm$^{-1}$)")
-# plt.ylabel("Amplitude (a.u.)")
-# plt.legend(["Kinetic", "Potential"])
-# plt.title("0.087 V/nm")
-# plt.xlim(13500, 16550)
-# plt.savefig(directory+'/'+'dependence_087.png')
+        # print(data.head())
+        # data.to_excel(directory+"_"+j+".xlsx")
+        """Вычисление спектров энергий"""
+        spectre_data = pd.DataFrame()
+        for energy_column in data.columns.values[1:]:
+            energies_psd = np.abs(sp.fftpack.fft(data[energy_column].tolist()))
+            n = len(energies_psd)
+            window = sp.signal.windows.gaussian(n, std=5000, sym=False)
+            module_w = energies_psd
+            b, a = sp.signal.butter(2, 0.05)
+            filtered = sp.signal.filtfilt(b, a, module_w)
+            fftFreq = sp.fftpack.fftfreq(len(data[energy_column].tolist()), 1/10**15)
+            i = fftFreq > 0
+            reverseCm = 1/((3*(10**10))/(fftFreq[i]))
+            spectre_data["FREQUENCY"] = reverseCm
+            spectre_data[(str(energy_column))] = filtered[i]
+        print(spectre_data.head())
+        spectre_data.to_excel(directory+"_"+j+"_spectre.xlsx")
