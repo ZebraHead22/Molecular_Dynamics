a39ef1f5fa5fa58cf2e66a792c279d8fbfc80021
ZebraHead22 <baranovma1993@gmail.com>
2023-07-05 14:32:32 +0300

    ts

--- students/main.py
+++ students/main.py
@@ -4,32 +4,31 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import re
 
-list_template = list()
 # глобальная цель - 6 датафреймов с Кин, Пот энергиями по трем АК
 # 1 прочитать в цикле каждый ДФ
 # 2 с помощью циклов получить список аминокислот
+# os.path.join
+# os.path.splitext
+# os.path.basename
 
-amino_acids = ['ff', 'trp', 'gly']
+list_template = list()
+amino_acids = list()
 
-os.path.join
-os.path.splitext
-os.path.basename
-
-#список ак - не знаю как сделать так, чтобы не выводилась "data"
 dir = os.getcwd()
 for address, dirs, files in os.walk(dir):
-    for dir in dirs:
-        name  = os.path.splitext(dir)
-        list_template = list(name) 
-        removed = list_template.pop(1)
-        
-        print(list_template) 
+    for i in dirs:
+        amino_acids.append(i)
 
-
-
-#чтение файлов  - не работает 
-for address, dirs, names in os.walk(dir):
-    for name in names:
-        df = pd.read_csv(name)
-        print(df.head())
-
+for acid in amino_acids:
+    amino_dir = dir+"/"+acid
+    for file in os.listdir(amino_dir):
+        filename, file_extension = os.path.splitext(file)
+        if file_extension == ".dat":
+            df = pd.read_csv(amino_dir+"/"+file, sep=" ", index_col=None)
+            #pd.insert( ... , 1)
+            #df["TIME"] = ...
+            #ts =  df["TS"].tolist()
+            df["TIME"] = [... for x in ts]
+            print(df.head())
+# в каждом TS 1 фс 
+# посчитать новые значения для TIME, удалить TS, вставить TIME
