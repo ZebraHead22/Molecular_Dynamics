3c0d84e477917e616ce577faf5ac38c3e4d876e0
ZebraHead22 <baranovma1993@gmail.com>
2024-01-05 23:47:45 +0300

    ready_2

--- phazeMod/phazeMod.py
+++ phazeMod/phazeMod.py
@@ -20,7 +20,7 @@ class MainApplication(QtWidgets.QMainWindow, ui.Ui_MainWindow):
     def your_update_function(self, value):
         self.time_wid.clear()
         self.spec_wid.clear()
-        self.lcdNumber.display(value/10)
+        self.lcdNumber.display(value)
 
         self.c1 = self.c1_dsb.value()
         self.c2 = self.c2_dsb.value()
@@ -29,14 +29,17 @@ class MainApplication(QtWidgets.QMainWindow, ui.Ui_MainWindow):
         self.add_fun = self.coeff_dial.value()
     
         # F = 0.1*10**15          #Частота сигнала
-        SR = 10**17        # No. of samples per second, 
+        SR = 10**18        # No. of samples per second, 
         Ts = 1./SR          # Sampling interval 
-        self.t = np.arange(0, 10**(-12), Ts)
+        self.t = np.arange(0, 0.5*10**(-12), Ts)
         
-        f = np.cos(2*np.pi*self.fff*self.t) #Функция для флуктуации
-        signal = np.cos(2*np.pi*self.freq*self.t+self.c1*f)
-        signal2 = (self.add_fun/10)*np.cos(2*np.pi*self.freq*self.t+self.c2*f)
-        self.s = signal*signal2
+        f = self.add_fun*np.cos(2*np.pi*self.fff*self.t) #Функция для флуктуации
+        signal = self.c1*np.cos(2*np.pi*self.freq*self.t+f)
+        signal2 = self.c2*np.cos(2*np.pi*self.freq*self.t+f)
+        if self.c2 == 0:
+            self.s = signal
+        else:
+            self.s = signal*signal2
        
         self.time_wid.setLabel('bottom', 'time', units='s')
         self.time_wid.setLabel('left', 'Amplitude (a.u.)', units=None)
@@ -50,13 +53,13 @@ class MainApplication(QtWidgets.QMainWindow, ui.Ui_MainWindow):
 
         self.spec_wid.setLabel('bottom', 'k', units='Hz')
         self.spec_wid.setLabel('left', 'Amplitude', units='a.u.')
-        self.spec_wid.setXRange(0, 2*10**15, padding=0)
+        self.spec_wid.setXRange(0, 0.5*10**15, padding=0)
         self.spec_wid.plot(self.freq, np.abs(self.X), pen='r')
 
     def plot(self):
         self.time_wid.clear()
         self.spec_wid.clear()
-        
+
         self.c1 = self.c1_dsb.value()
         self.c2 = self.c2_dsb.value()
         self.freq = self.freq_dsb.value()*(10**12)
@@ -64,14 +67,17 @@ class MainApplication(QtWidgets.QMainWindow, ui.Ui_MainWindow):
         self.add_fun = self.coeff_dial.value()
     
         # F = 0.1*10**15          #Частота сигнала
-        SR = 10**17        # No. of samples per second, 
+        SR = 10**18        # No. of samples per second, 
         Ts = 1./SR          # Sampling interval 
-        self.t = np.arange(0, 10**(-12), Ts)
+        self.t = np.arange(0, 0.5*10**(-12), Ts)
         
-        f = np.cos(2*np.pi*self.fff*self.t) #Функция для флуктуации
-        signal = np.cos(2*np.pi*self.freq*self.t+self.c1*f)
-        signal2 = (self.add_fun/10)*np.cos(2*np.pi*self.freq*self.t+self.c2*f)
-        self.s = signal*signal2
+        f = self.add_fun*np.cos(2*np.pi*self.fff*self.t) #Функция для флуктуации
+        signal = self.c1*np.cos(2*np.pi*self.freq*self.t+f)
+        signal2 = self.c2*np.cos(2*np.pi*self.freq*self.t+f)
+        if self.c2 == 0:
+            self.s = signal
+        else:
+            self.s = signal*signal2
        
         self.time_wid.setLabel('bottom', 'time', units='s')
         self.time_wid.setLabel('left', 'Amplitude (a.u.)', units=None)
@@ -85,7 +91,7 @@ class MainApplication(QtWidgets.QMainWindow, ui.Ui_MainWindow):
 
         self.spec_wid.setLabel('bottom', 'k', units='Hz')
         self.spec_wid.setLabel('left', 'Amplitude', units='a.u.')
-        self.spec_wid.setXRange(0, 2*10**15, padding=0)
+        self.spec_wid.setXRange(0, 0.5*10**15, padding=0)
         self.spec_wid.plot(self.freq, np.abs(self.X), pen='r')
 
 def main():
