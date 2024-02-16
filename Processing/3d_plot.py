d17049bd98b36c4c06efc217ae929ef622e35049
ZebraHead22 <baranovma1993@gmail.com>
2024-02-16 15:14:39 +0300

    add equal distance between yticks

--- Processing/3d_plot.py
+++ Processing/3d_plot.py
@@ -14,6 +14,7 @@ frequencies = list()
 energy_type = ['KINETIC', 'POTENTIAL']
 field_amplitudes = [0.0435,	0.087,	0.1305,	0.174,	0.2175,
                     0.261,	0.3045,	0.348,	0.3915,	0.435,	0.4785,	0.522]
+norm_freq = [0 , 100, 200, 300, 400, 500]
 
 slice_of_energy = pd.DataFrame()
 directory = os.getcwd()
@@ -22,7 +23,10 @@ for dir in os.listdir(directory):
     if os.path.isdir(dir) == True:
         frequencies.append(int(dir))
 frequencies = sorted(list(set(frequencies)))
+ticks = [str(x) for x in frequencies]
+ticks.insert(0, "0")
 print('Frequencies is ', frequencies)  
+print('Frequencies is ', ticks)  
 
 for address, dirs, names in os.walk(directory):
     for name in names:
@@ -39,7 +43,8 @@ for i in amino_acids:
         fig = plt.figure()
         ax = fig.add_subplot(projection='3d')
         out = pd.DataFrame()
-        for f in frequencies:
+        for a in range(len(frequencies)):
+            f = frequencies[a]
             data = pd.DataFrame()
             files = os.listdir(directory+"/"+str(f)+'/'+i)
             files = sorted(files)
@@ -60,15 +65,14 @@ for i in amino_acids:
             X_Y_Spline = make_interp_spline(field_amplitudes, last_moment_energies)
             X_ = np.linspace((np.array(field_amplitudes)).min(), (np.array(field_amplitudes)).max(), 500)
             Y_ = X_Y_Spline(X_)
-            # plt.plot(X_, Y_,  linewidth=2)
-            ax.plot(X_, Y_, f, zdir='y', marker = 'o', markersize=0.3,  linewidth = 2)
-        # ax.set_yticks(frequencies)
+            ax.plot(X_, Y_, norm_freq[a], zdir='y', marker = 'o', markersize=0.3,  linewidth = 2)
         ax.set_title((str(i)).upper()+' '+str(j))
         ax.set_xlabel('Field Amplitude (V/nm)')
         ax.set_ylabel('Field Frequency ($cm^{-1}$)')
         ax.set_zlabel('Amplitude (eV)')
+        ax.set_yticklabels(ticks)
         ax.grid()
         plt.legend([str(x)+'$cm^{-1}$' for x in frequencies])
-        # plt.yticks(rotation=45)
         plt.show()
-        # ax.figure.savefig(directory+'/'+str(i)+'_'+str(j).lower()+".png")
+        # fig.set_size_inches(10, 10)
+        # ax.figure.savefig(directory+'/'+str(i)+'_'+str(j).lower()+".png", dpi=1200)
