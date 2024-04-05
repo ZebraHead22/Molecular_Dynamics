3ece7717fa8cb6414efa35367148c156702e4f86
Maksim Baranov <baranovma1993@gmail.com>
2024-04-05 12:14:47 +0300

    family

--- Processing/3d_plot_hist.py
+++ Processing/3d_plot_hist.py
@@ -2,32 +2,39 @@ import os
 import re
 import numpy as np
 import pandas as pd
-import openpyxl
+from matplotlib import pyplot as plt
 
-FIELD_FREQUENCIES = list()
-DATA = pd.DataFrame()
+legend = list()
+for folder in os.listdir(os.getcwd()):
+    print(f"Folder is {folder}")
+    legend.append(str(folder))
+    value_dict = {}
+    for data_file in os.listdir(os.getcwd()+'/'+folder):
+        file_name, file_extension = os.path.splitext(os.getcwd()+'/'+folder+'/'+data_file)
+        if file_extension == '.dat':
+            dot = re.search(r'\d+', str(os.path.basename(file_name)))
+            dot = dot.group(0)
+            # print(f"Filename is {dot}")
+            data_df = pd.read_csv(os.getcwd()+'/'+folder+'/'+data_file, sep=' ', index_col=None)
+            data_df.rename(columns={'0.0': 'Freq', '0.0.1': 'Amp'}, inplace=True)
+            max_amp_value = round((data_df['Amp'].max())*(10**2), 2) # mul to 100
+            # print(f"Max value is {max_amp_value}")
+            value_dict[int(dot)] = max_amp_value
 
-for address, dirs, names in os.walk(os.getcwd()):
-    for name in names:
-        filename, file_extension = os.path.splitext(name)
-        if file_extension == ".dat":
-            field_frequency = re.search(r'\d+', filename)
-            FIELD_FREQUENCIES.append(int(field_frequency.group(0)))
+    sorted_list = sorted(value_dict.items())
 
-            df = pd.read_csv(os.getcwd()+'/'+name, sep=' ')
-            df.dropna(how='all', axis=1, inplace=True)
-            df.rename(columns={'#': 'frame', 'Unnamed: 2': 'dip_x', 'Unnamed: 4': 'dip_y',
-                            'Unnamed: 6': 'dip_z', 'Unnamed: 8': '|dip|'}, inplace=True)
-            df.insert(0, "Time", (df['frame'] * 5)/1000)
-            if filename == '0':
-                DATA['No_Field'] = df['|dip|']
-            else:
-                DATA[filename] = df['|dip|']
+    sorted_dict = {}
+    for key, value in sorted_list:
+        sorted_dict[key] = value
 
-FIELD_FREQUENCIES.sort()
-FIELD_FREQUENCIES = [str(x) for x in FIELD_FREQUENCIES]
-FIELD_FREQUENCIES[0] = 'No_Field'
-DATA = DATA[FIELD_FREQUENCIES]
-DATA.insert(0, 'Time_(ps)', DATA.index * 5 /1000) 
-DATA = DATA.dropna(axis=0)
-DATA.to_excel(os.getcwd()+'/5mol_data.xlsx', 'dip_data')
+    print(f"Dots on the graph:\n {sorted_dict}")
+
+    plt.plot(np.array(list(sorted_dict.keys())), np.array(list(sorted_dict.values())))
+
+plt.grid()
+plt.legend(legend)
+plt.xticks(np.arange(0, 19, 1))
+plt.xlabel('N')
+plt.ylabel('Max Spectral Density (a.u. ×$10^{2}$)')
+# plt.show()
+plt.savefig(os.getcwd()+'/family.png')
