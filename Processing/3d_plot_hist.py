275843e31294843e4ee6e57de33cc7ee3f3977cf
Maksim Baranov <baranovma1993@gmail.com>
2024-04-09 14:04:47 +0300

    3в

--- Processing/3d_plot_hist.py
+++ Processing/3d_plot_hist.py
@@ -4,13 +4,12 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-legend = list()
-markers = iter(['+', 'o', 's'])
+main_df = pd.DataFrame()
+markers = iter(['s', 'o'])
+
 for folder in os.listdir(os.getcwd()):
     if os.path.isdir(folder):
         print(f"Folder is {folder}")
-        legend.append(str(folder))
-        legend.append(str(folder + " approximation"))
         value_dict = {}
         for data_file in os.listdir(os.getcwd()+'/'+folder):
             file_name, file_extension = os.path.splitext(
@@ -30,26 +29,34 @@ for folder in os.listdir(os.getcwd()):
         sorted_dict = {}
         for key, value in sorted_list:
             sorted_dict[key] = value
-        print(f"Dots on the graph:\n {sorted_dict}")
-        plt.scatter(np.array(list(sorted_dict.keys())), np.array(
-            # Scatter Plot
-            list(sorted_dict.values())), s=25, c='black', marker=next(markers))
-        a = np.polyfit(np.log(np.array(list(sorted_dict.keys()))), np.array(
-            list(sorted_dict.values())), 1)  # Approximation coefficients
-        y = a[0] * np.log(np.array(list(sorted_dict.keys()))
-                          ) + a[1]  # Approximation
-        plt.plot(np.array(list(sorted_dict.keys())),
-                 y, 'k--', lw=1)  # Approximate plot
-    else:
-        print(f"{folder} is not a dir")
+        print(sorted_dict)
+
+        main_df['N'] = list(sorted_dict.keys())
+        main_df[str(folder)] = list(sorted_dict.values())
+
+main_df['average'] = main_df[['plus_circle_wave', 'minus_circle_wave']].mean(axis=1)
+main_df = main_df.drop([17])
+print(main_df.head())
 
 max_values = [1.62, 1.95, 4.14, 5.36, 6.12]
-captions = ['1 mol', '2 mol', '3 mol', '4 mol', '5 mol']
+captions = ['I', 'II', 'III', 'IV', 'V']
 
 for max_value in max_values:
     plt.plot(18, max_value, '*', c='red')
 for i, txt in enumerate(captions):
-    plt.annotate(txt, (18, max_values[i]), xytext=(18.3, max_values[i]-0.1))
+    plt.annotate(txt, (18, max_values[i]), xytext=(18.3, max_values[i]-0.1), fontsize=14)
+
+plt.scatter(np.array(main_df['N'].tolist()), np.array(main_df['flat_wave'].tolist()), s=25, c='black', marker='s') # Scatter Plot
+a = np.polyfit(np.log(np.array(list(main_df['N'].tolist()))), np.array(list(main_df['flat_wave'].tolist())), 1)  # Approximation coefficients
+y = a[0] * np.log(np.array(list(main_df['N'].tolist()))) + a[1]  # Approximation
+plt.plot(np.array(list(main_df['N'].tolist())), y, 'k--', lw=1)  # Approximate plot
+plt.annotate('2', (0.9, 1.5), fontsize=14)
+
+plt.scatter(np.array(main_df['N'].tolist()), np.array(main_df['average'].tolist()), s=25, c='black', marker='o') # Scatter Plot
+a = np.polyfit(np.log(np.array(list(main_df['N'].tolist()))), np.array(list(main_df['average'].tolist())), 1)  # Approximation coefficients
+y = a[0] * np.log(np.array(list(main_df['N'].tolist()))) + a[1]  # Approximation
+plt.plot(np.array(list(main_df['N'].tolist())),y, 'k--', lw=1)  # Approximate plot
+plt.annotate('1', (0.9, 4.4), fontsize=14)
 
 plt.grid()
 plt.xticks(np.arange(0, 22, 2))
