0d4e98e3ee7e0022ef22dada647f03ee80430f03
Maksim Baranov <baranovma1993@gmail.com>
2024-04-23 12:51:15 +0300

    for n=15, 18

--- Processing/dipoles.py
+++ Processing/dipoles.py
@@ -1,5 +1,4 @@
 # Здесь считаем полный дипольный момент для нескольких реализаций 
-
 import os
 import re
 import pandas as pd
@@ -14,19 +13,23 @@ for i in os.listdir(os.getcwd()):
     if file_extension == ".dat":
         number = re.search(r'\d+', filename)
         number = number.group(0)
+        print(number)
         df = pd.read_csv(os.getcwd()+'/'+i, sep=' ')
         df.dropna(how='all', axis=1, inplace=True)
         df.rename(columns={'#': 'frame', 'Unnamed: 2': 'dip_x', 'Unnamed: 4': 'dip_y',
                   'Unnamed: 6': 'dip_z', 'Unnamed: 8': '|dip|'}, inplace=True)
-        data['N='+number] = df['|dip|']
+        data['N='+number] = df['|dip|'] 
     
+
 data = data.reindex(sorted(data.columns), axis=1)
 data.insert(0, 'Time (ps)', time)
 data = data.set_index('Time (ps)')
-data = data.drop(columns=['N=3', 'N=4', 'N=7', 'N=10', 'N=13', 'N=14', 'N=16', 'N=17', 'N=5', 'N=8', 'N=11'], axis=1)
+print(data.head())    
+# # data = data.drop(columns=['N=3', 'N=4', 'N=5', 'N=7', 'N=8', 'N=10', 'N=11', 'N=13', 'N=14', 'N=16', 'N=17'], axis=1)
+# # data = data.drop(columns=['N=1', 'N=2', 'N=6', 'N=9', 'N=12'], axis=1)
 data.plot(linewidth = 1, legend=False)
 plt.grid()
 plt.ylabel('Dipole Moment (D)')
-
+# plt.legend(data.columns)
 # plt.show()
-plt.savefig(os.getcwd()+'/'+'dipoles_varN.png', dpi=1200)
+plt.savefig(os.getcwd()+'/'+'dipoles_varN.png', dpi=600)
