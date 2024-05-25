33431dd1c535e72a53c9d9fb3d6bb28a8eb68527
Maksim Baranov <baranovma1993@gmail.com>
2024-05-25 12:01:17 +0300

    polar

---
+++ Processing/polar.py
@@ -0,0 +1,44 @@
+# Здесь считаем дипольные моменты в полярных координатах. строим годограф
+import os
+import re
+import time
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+for i in os.listdir(os.getcwd()):
+    filename, file_extension = os.path.splitext(os.getcwd()+'/'+i)
+    if file_extension == ".dat":
+        # number = re.search(r'\d+', filename)
+        # number = number.group(0)
+        df = pd.read_csv(os.getcwd()+'/'+i, sep=' ')
+        df.dropna(how='all', axis=1, inplace=True)
+        df.rename(columns={'#': 'frame', 'Unnamed: 2': 'dip_x', 'Unnamed: 4': 'dip_y',
+                  'Unnamed: 6': 'dip_z', 'Unnamed: 8': '|dip|'}, inplace=True)
+        
+        
+
+        df.insert(5, 'r', (df['dip_x']**2+df['dip_y']**2)**(1/2))
+        theta = list()
+        for i, row in df.iterrows():
+            if df.loc[i, 'dip_x'] > 0 and df.loc[i, 'dip_y'] >= 0:
+                theta.append(float(np.arctan(df.loc[i, 'dip_y']/df.loc[i, 'dip_x'])))
+            elif df.loc[i, 'dip_x'] > 0 and df.loc[i, 'dip_y'] < 0:
+                theta.append(float(np.arctan(df.loc[i, 'dip_y']/df.loc[i, 'dip_x'])) + 2*np.pi)
+            elif df.loc[i, 'dip_x'] < 0:
+                theta.append(float(np.arctan(df.loc[i, 'dip_y']/df.loc[i, 'dip_x'])) + np.pi)
+            elif df.loc[i, 'dip_x'] == 0 and df.loc[i, 'dip_y'] > 0:
+                theta.append(float(np.pi/2))
+            elif df.loc[i, 'dip_x'] == 0 and df.loc[i, 'dip_y'] < 0:
+                theta.append(float(3*np.pi/2))
+            if df.loc[i, 'dip_x'] == 0 and df.loc[i, 'dip_y'] == 0:
+                theta.append(None)
+
+        df.insert(6, 'theta', theta)
+        
+        print(df.head())
+
+        plt.gcf().clear()
+        plt.polar(np.array(df['theta'].tolist()), np.array(df['r'].tolist()))
+        plt.grid(True)
+        plt.show()
