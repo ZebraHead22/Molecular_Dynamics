63486f8391592763c801c80ad4e7996043044904
ZebraHead22 <baranovma1993@gmail.com>
2024-07-18 13:16:50 +0300

    21

--- Processing/3d_plot.py
+++ Processing/3d_plot.py
@@ -78,11 +78,87 @@ from scipy.interpolate import make_interp_spline
 #         fig.set_size_inches(10, 10)
 #         ax.figure.savefig(directory+'/'+str(i)+'_'+str(j).lower()+".png", dpi=1200)
 
-files = os.listdir(os.getcwd())
-for file in files:
-    filename, file_extension = os.path.splitext(file)
-    if file_extension == '.txt':
-        case = re.search(r'\w+_\d+', filename)
-        case = case.group(0)
-        
-    
+def make_spectres():
+    zero_data = pd.read_csv(
+        '/Users/max/Yandex.Disk.localized/NAMD/basic_ak_no_field/ala.dat', delimiter=' ', index_col=None)
+    zero_data.rename(columns={'0.0': 'Frequency',
+                     '0.0.1': 'Amplitude'}, inplace=True)
+    zero_data.insert(2, 'Amp×104', zero_data['Amplitude']*(10**4))
+
+    folder = os.getcwd()
+    files = os.listdir(folder)
+    # file = open("frequencies.txt", "w")
+    file = open(os.path.basename(folder) + "_ratio.txt", "w")
+    for i in files:
+        filename, file_extension = os.path.splitext(os.getcwd()+'/'+i)
+        if file_extension == ".dat":
+
+            field_freq = re.search(r'\d{3,}', str(os.path.basename(filename)))
+            if field_freq:
+                field_freq = field_freq.group(0)
+            else:
+                field_freq = 'no_field'
+
+            df = pd.read_csv(os.getcwd()+'/'+i, delimiter=' ', index_col=None)
+            df.rename(columns={'0.0': 'Frequency',
+                      '0.0.1': 'Amplitude'}, inplace=True)
+            df.insert(2, 'Amp×104', df['Amplitude']*(10**4))
+
+            # df.iloc[:1050] = np.nan
+            # df.iloc[45000:] = np.nan
+            # df = df[df['Frequency'] > 10]
+            # df = df.drop(df[df['Frequency'] > 1000].index)
+
+            dfFreq = np.array(df['Frequency'].tolist())
+            dfAmp = np.array(df['Amp×104'].tolist())
+            # dfAmpRev = list(1 - i for i in dfAmp) #Вычитаем из единицы
+
+            # Ищем точку максимума на резонансной частоте
+            G = 50
+            max_amp_freq = df.loc[df['Amplitude'].where((df['Frequency'] < (int(
+                field_freq) + G)) & (df['Frequency'] > (int(field_freq) - G))).idxmax(), 'Frequency']
+            max_amp = df.loc[df['Amplitude'].where((df['Frequency'] < (int(
+                field_freq) + G)) & (df['Frequency'] > (int(field_freq) - G))).idxmax(), 'Amp×104']
+
+            max_amp_no_field = zero_data.loc[zero_data['Amplitude'].where((zero_data['Frequency'] < (float(
+                max_amp_freq) + 1)) & (zero_data['Frequency'] > (float(max_amp_freq) - 1))).idxmax(), 'Amp×104']
+            max_amp_freq_no_field = zero_data.loc[zero_data['Amplitude'].where((zero_data['Frequency'] < (float(
+                max_amp_freq) + 1)) & (zero_data['Frequency'] > (float(max_amp_freq) - 1))).idxmax(), 'Frequency']
+
+            # Ищем точку максимума без учета частоты поля
+            # max_amp_freq = df.loc[df['Amplitude'].idxmax(), 'Frequency']
+            # max_amp = df.loc[df['Amplitude'].idxmax(), 'Amp×104']
+
+            # mess = 'Field ' + \
+            #     str(field_freq) + " : " + str(max_amp_freq) + \
+            #     ' - ' + str(max_amp) + '\n'
+
+            ratio = max_amp / max_amp_no_field
+            ratio = round(ratio, 2)
+            mess = str(field_freq) + " : " + str(ratio) + '\n'
+            print(mess)
+            # Пишем пики в файл
+            file.write(mess)
+            # Строим графики
+            plt.gcf().clear()
+            plt.plot(dfFreq, dfAmp, linewidth=1, c='black')  # Обычный графики
+            plt.grid()
+            plt.xlabel('Frequency, $cm^{-1}$')
+            plt.ylabel('Spectral Density (a.u. ×$10^{4}$)')
+            # plt.xlabel('Частота, $cm^{-1}$')
+            # plt.ylabel('Амплитуда, отн.ед. ×$10^{4}$')
+            # plt.savefig(filename+'_main.png')
+            plt.xlim(-50, 1050)
+            plt.ylim(-max_amp*0.05, max_amp + max_amp*0.1)
+            # plt.savefig(filename+'_u1k.png')
+
+            # Оконные автоматические графики
+            D = 10  # Смещение для окон и подписей
+            plt.xlim(float(max_amp_freq) - D, float(max_amp_freq) + D)
+            plt.ylim(-max_amp*0.03, max_amp + max_amp*0.5)
+            plt.annotate(str(round(max_amp_freq, 2)), xy=(float(max_amp_freq), float(max_amp)), xytext=(float(
+                max_amp_freq) + 0.5*D, float(max_amp) + float(max_amp)*0.05), arrowprops=dict(facecolor='red', shrink=0.05), fontsize=14)
+            # plt.savefig(filename+'_window.png')
+    file.close()
+
+make_spectres()
